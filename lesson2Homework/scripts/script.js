4b95f5454bf81411a9f1d14ab75e54b5d4d27612
jdilla19 <jtilkavashvili@gmail.com>
2024-01-23 13:32:25 +0400

    add work 2/3/4

--- lesson2Homework/scripts/script.js
+++ lesson2Homework/scripts/script.js
@@ -1,12 +1,46 @@
-let x = 4;
+var myArray = [1, 2, 3, 4, 5];
 
-var array = [1, 2, 3, 4, 5];
+if (Array.isArray(myArray)) {
+    console.log("myArray is an array");
+  } else {
+    console.log("myArray is not an array");
+  }
 
-if (array.includes(x)) {
-    console.log("x მოიძებნა მასივში");
-} else {
-    console.log("x არ მოიძებნა მასივში");
-}
+
+let names = ["Nika", "Gio" , "Saba" , "Mari" , "Ani"];
+console.log(names[0]);
+
+
+
+let name = ["Nika", "Gio" , "Saba" , "Mari" , "Ani"];
+let lastElement = name.pop();
+console.log(lastElement);
+
+
+
+
+
+let color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+let o = ["th","st","nd","rd"];
+
+let firstColor = color[0];
+let firstO = o[1];
+console.log("1" + firstO + " choice is " + firstColor);
+
+
+let secondColor = color[1];
+let secondO = o[2];
+console.log("2" + secondO + " choice is " + secondColor);
+
+let thirdColor = color[2];
+let thirdO = o[3];
+console.log("3" + thirdO + " choice is " + thirdColor);
+
+
+
+let sakheli = "Mate"
+let gvari = "gotua"
+
 
 
 
